954774b5d4107ee004041c3c380a2b5175477820
Shritesh Aher <shriteshaher@gmail.com>
2024-12-24 17:24:57 +0530

    Some Changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,10 @@
-import { ChangeDetectorRef, Component, inject, ViewChild, viewChild } from '@angular/core';
+import {
+  ChangeDetectorRef,
+  Component,
+  inject,
+  ViewChild,
+  viewChild,
+} from '@angular/core';
 import { MatIconRegistry } from '@angular/material/icon';
 import { DomSanitizer } from '@angular/platform-browser';
 import { NavBarComponent } from './shared-component/nav-bar/nav-bar.component';
@@ -8,10 +14,10 @@ import { TasksService } from './servicess/tasks.service';
 import { TaskDetail } from './servicess/DAO/TaskDetail';
 import { MatSnackBar } from '@angular/material/snack-bar';
 
-type flagDetails={
-  isFilterForm:boolean,
-  isAddTaskForm:boolean
-}
+type flagDetails = {
+  isFilterForm: boolean;
+  isAddTaskForm: boolean;
+};
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -33,16 +39,15 @@ export class AppComponent {
     // this.matIconRegistry.addSvgIcon('filter',this.sanitizer.bypassSecurityTrustResourceUrl("assets/images/funnel.svg"))
   }
   ngOnInit() {
-    this.getTaskDetails("");
-    
+    this.getTaskDetails('');
   }
   ngAfterViewInit() {
-    this.navBar.openModalEvent.subscribe((event:flagDetails) => {
+    this.navBar.openModalEvent.subscribe((event: flagDetails) => {
       if (event) {
-      if(event.isAddTaskForm){
-        this.submitTaskDetails()
+        if (event.isAddTaskForm) {
+          this.submitTaskDetails();
+        }
       }
-    }
     });
   }
 
@@ -51,83 +56,79 @@ export class AppComponent {
       width: '400px',
       height: '470px',
     });
-    
-    return dialogRef
+
+    return dialogRef;
   }
 
-  getTaskDetails(searchText:any) {
-    console.log(searchText)
-    if(!searchText){
-    this.taskSerivice.getTaskDetails("").subscribe((result) => {
-      console.log(result);
-      this.task_details = result;
-      
-    });}else{
-      this.taskSerivice.getTaskDetails(searchText).subscribe((result) => {
+  getTaskDetails(searchText: any) {
+    console.log(searchText);
+    if (!searchText) {
+      this.taskSerivice.getTaskDetails('').subscribe((result) => {
+        console.log(result);
         this.task_details = result;
-      },(err)=>{
-        alert("not found")
-        this.task_details=[]
       });
+    } else {
+      this.taskSerivice.getTaskDetails(searchText).subscribe(
+        (result) => {
+          this.task_details = result;
+        },
+        (err) => {
+          alert('not found');
+          this.task_details = [];
+        }
+      );
     }
   }
 
   deleteCard(card_id: string) {
     this.taskSerivice.deleteTask(card_id).subscribe((res) => {
-      alert("Task Deleted Successfully")
-      this.getTaskDetails("");
+      alert('Task Deleted Successfully');
+      this.getTaskDetails('');
     });
   }
 
-  viewTaskDataModal(card_data:TaskDetail){
-    const dialogRef=this.openModalAddTaskForm()
+  viewTaskDataModal(card_data: TaskDetail) {
+    const dialogRef = this.openModalAddTaskForm();
     dialogRef.componentInstance.addTaskFormGroup.setValue({
-      task_title:card_data.task_title,
-      task_description:card_data.task_description,
-      priority:card_data.priority,
-      status:card_data.status
-    })
-    dialogRef.componentInstance.addTaskFormGroup.disable()
-    dialogRef.componentInstance.isDisabled=true
-    this.cdr.detectChanges()
-
+      task_title: card_data.task_title,
+      task_description: card_data.task_description,
+      priority: card_data.priority,
+      status: card_data.status,
+    });
+    dialogRef.componentInstance.addTaskFormGroup.disable();
+    dialogRef.componentInstance.isDisabled = true;
+    this.cdr.detectChanges();
   }
 
-  submitTaskDetails(){
-    const dialogRef=this.openModalAddTaskForm()
+  submitTaskDetails() {
+    const dialogRef = this.openModalAddTaskForm();
     dialogRef.componentInstance.submitValueEventEmitter.subscribe((result) => {
       this.taskSerivice.addTask(result).subscribe((result) => {
-        alert("Task Added Successfully")
-       if(result){
-        this.getTaskDetails("");
-        dialogRef.close();
-       }
+        alert('Task Added Successfully');
+        if (result) {
+          this.getTaskDetails('');
+          dialogRef.close();
+        }
       });
     });
   }
 
-  updateCardDataModal(card_data:TaskDetail){
-    const dialogRef=this.openModalAddTaskForm()
+  updateCardDataModal(card_data: TaskDetail) {
+    const dialogRef = this.openModalAddTaskForm();
     dialogRef.componentInstance.addTaskFormGroup.setValue({
-      task_title:card_data.task_title,
-      task_description:card_data.task_description,
-      priority:card_data.priority,
-      status:card_data.status
-    })
-    dialogRef.componentInstance.isUpdate=true
-    dialogRef.componentInstance.updateValueEventEmiter.subscribe(
-      (result)=>{
-        result.id=card_data.id
-       this.taskSerivice.updateTask(result.id,result).subscribe(
-        (res)=>{
-          alert("Task Updated Successfully")
-          this.getTaskDetails("")
-          dialogRef.close()
-        }
-       )
-      }
-    )
+      task_title: card_data.task_title,
+      task_description: card_data.task_description,
+      priority: card_data.priority,
+      status: card_data.status,
+    });
+    dialogRef.componentInstance.isUpdate = true;
+    dialogRef.componentInstance.updateValueEventEmiter.subscribe((result) => {
+      result.id = card_data.id;
+      this.taskSerivice.updateTask(result.id, result).subscribe((res) => {
+        alert('Task Updated Successfully');
+        this.getTaskDetails('');
+        dialogRef.close();
+      });
+    });
   }
-
- 
 }
